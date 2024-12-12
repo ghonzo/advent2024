eb4428f4ee92db362813cd14818609648dc3bdc5
Eric Lunt <eric@lunt.software>
2024-12-12 10:49:24 -0600

    Day 12 part 2

--- day12/main.go
+++ day12/main.go
@@ -10,17 +10,59 @@ import (
 
 // Day 12: Garden Groups
 // Part 1 answer: 1549354
-// Part 2 answer:
+// Part 2 answer: 937032
 func main() {
 	fmt.Println("Advent of Code 2024, Day 12")
 	entries := common.ReadStringsFromFile("input.txt")
 	fmt.Printf("Part 1: %d\n", part1(entries))
-	//fmt.Printf("Part 2: %d\n", part2(entries))
+	fmt.Printf("Part 2: %d\n", part2(entries))
+}
+
+type fence struct {
+	pos, dir common.Point
 }
 
 type region struct {
-	points    mapset.Set[common.Point]
-	perimeter int
+	points mapset.Set[common.Point]
+	fence  mapset.Set[fence]
+}
+
+func (r region) area() int {
+	return r.points.Cardinality()
+}
+
+func (r region) perimeter() int {
+	return r.fence.Cardinality()
+}
+
+func (r region) sides() int {
+	var sides int
+	fencesToCount := r.fence.Clone()
+	for !fencesToCount.IsEmpty() {
+		f, _ := fencesToCount.Pop()
+		sides++
+		if f.dir == common.N || f.dir == common.S {
+			// travel east and west
+			removeFences(fencesToCount, f, common.E)
+			removeFences(fencesToCount, f, common.W)
+		} else {
+			removeFences(fencesToCount, f, common.N)
+			removeFences(fencesToCount, f, common.S)
+		}
+	}
+	return sides
+}
+
+func removeFences(fences mapset.Set[fence], f fence, dir common.Point) {
+	for {
+		// f is a copy we can modify it
+		f.pos = f.pos.Add(dir)
+		if fences.Contains(f) {
+			fences.Remove(f)
+		} else {
+			return
+		}
+	}
 }
 
 func part1(entries []string) int {
@@ -32,7 +74,7 @@ func part1(entries []string) int {
 			continue
 		}
 		r := findRegion(grid, p)
-		total += r.points.Cardinality() * r.perimeter
+		total += r.area() * r.perimeter()
 		visited = visited.Union(r.points)
 	}
 	return total
@@ -42,6 +84,7 @@ func findRegion(grid common.Grid, start common.Point) region {
 	var r region
 	plotType := grid.Get(start)
 	r.points = mapset.NewThreadUnsafeSet[common.Point]()
+	r.fence = mapset.NewThreadUnsafeSet[fence]()
 	pointsToVisit := mapset.NewThreadUnsafeSet[common.Point](start)
 	for !pointsToVisit.IsEmpty() {
 		// Check each of the surrounding points
@@ -52,7 +95,7 @@ func findRegion(grid common.Grid, start common.Point) region {
 				continue
 			}
 			if v, _ := grid.CheckedGet(p); v != plotType {
-				r.perimeter++
+				r.fence.Add(fence{currentPt, p.Sub(currentPt)})
 			} else {
 				pointsToVisit.Add(p)
 			}
@@ -63,15 +106,15 @@ func findRegion(grid common.Grid, start common.Point) region {
 
 func part2(entries []string) int {
 	var total int
-	left := make([]int, len(entries))
-	rightMap := make(map[int]int)
-	for i, line := range entries {
-		values := common.ConvertToInts(line)
-		left[i] = values[0]
-		rightMap[values[1]]++
-	}
-	for _, l := range left {
-		total += l * rightMap[l]
+	grid := common.ArraysGridFromLines(entries)
+	visited := mapset.NewThreadUnsafeSet[common.Point]()
+	for p := range grid.AllPoints() {
+		if visited.Contains(p) {
+			continue
+		}
+		r := findRegion(grid, p)
+		total += r.area() * r.sides()
+		visited = visited.Union(r.points)
 	}
 	return total
 }
