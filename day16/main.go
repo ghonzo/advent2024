d6038bd4ccf980c183a521dfa0617370220524cb
Eric Lunt <eric@lunt.software>
2024-12-16 09:26:17 -0600

    Day 16 first cleanup

--- day16/main.go
+++ day16/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/oleiade/lane/v2"
 )
 
-// Day 16:
+// Day 16: Reindeer Maze
 // Part 1 answer: 95444
 // Part 2 answer: 513
 func main() {
@@ -21,6 +21,71 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(entries))
 }
 
+// This is all the state we need for part 1
+type posAndDir struct {
+	pos, dir common.Point
+}
+
+func part1(entries []string) int {
+	maze, start, end := readMaze(entries)
+	minCost := make(map[posAndDir]int)
+	pq := lane.NewMinPriorityQueue[posAndDir, int]()
+	pq.Push(posAndDir{start, common.E}, 0)
+	for !pq.Empty() {
+		curPad, score, _ := pq.Pop()
+		// Finish state
+		if curPad.pos == end {
+			return score
+		}
+		// Find all the possible new states
+		// Move forward
+		p := curPad.pos.Add(curPad.dir)
+		if v, ok := maze.CheckedGet(p); ok && v != '#' {
+			newPad := posAndDir{p, curPad.dir}
+			newScore := score + 1
+			if potentialPath(minCost, newPad, newScore, false) {
+				pq.Push(newPad, newScore)
+			}
+		}
+		// Turn right
+		newPad := posAndDir{curPad.pos, curPad.dir.Right()}
+		newScore := score + 1000
+		if potentialPath(minCost, newPad, newScore, false) {
+			pq.Push(newPad, newScore)
+		}
+		// Turn left
+		newPad = posAndDir{curPad.pos, curPad.dir.Left()}
+		if potentialPath(minCost, newPad, newScore, false) {
+			pq.Push(newPad, newScore)
+		}
+	}
+	panic("failed")
+}
+
+// Returns the grid, start point, and end point
+func readMaze(entries []string) (maze common.Grid, start, end common.Point) {
+	maze = common.ArraysGridFromLines(entries)
+	for p := range maze.AllPoints() {
+		switch maze.Get(p) {
+		case 'S':
+			start = p
+		case 'E':
+			end = p
+		}
+	}
+	return
+}
+
+// If this represents a potential minimum cost path, return true
+func potentialPath(minCost map[posAndDir]int, pad posAndDir, score int, includeEqual bool) bool {
+	if v, ok := minCost[pad]; !ok || score < v || (includeEqual && score == v) {
+		minCost[pad] = score
+		return true
+	}
+	return false
+}
+
+// For part2, we need to retain the entire path
 type state struct {
 	path []common.Point
 	dir  common.Point
@@ -30,82 +95,15 @@ func (s state) pos() common.Point {
 	return s.path[len(s.path)-1]
 }
 
-type posAndDir struct {
-	pos, dir common.Point
-}
-
 func (s state) asPosAndDir() posAndDir {
 	return posAndDir{s.pos(), s.dir}
 }
 
-func part1(entries []string) int {
-	maze := common.ArraysGridFromLines(entries)
-	var start, end common.Point
-	for p := range maze.AllPoints() {
-		switch maze.Get(p) {
-		case 'S':
-			start = p
-		case 'E':
-			end = p
-		}
-	}
-	minCost := make(map[posAndDir]int)
-	pq := lane.NewMinPriorityQueue[state, int]()
-	pq.Push(state{[]common.Point{start}, common.E}, 0)
-	for !pq.Empty() {
-		curState, score, _ := pq.Pop()
-		// Finish state
-		if curState.pos() == end {
-			return score
-		}
-		// Find all the possible new states
-		// Move forward
-		newPath := slices.Clone(curState.path)
-		newPath = append(newPath, curState.pos().Add(curState.dir))
-		newState := state{newPath, curState.dir}
-		newScore := score + 1
-		if v, ok := maze.CheckedGet(newState.pos()); ok && v != '#' {
-			if possibleNewState(minCost, newState, newScore) {
-				pq.Push(newState, newScore)
-			}
-		}
-		// Turn right
-		newState = state{curState.path, curState.dir.Right()}
-		newScore = score + 1000
-		if possibleNewState(minCost, newState, newScore) {
-			pq.Push(newState, newScore)
-		}
-		// Or turn left
-		newState = state{curState.path, curState.dir.Left()}
-		if possibleNewState(minCost, newState, newScore) {
-			pq.Push(newState, newScore)
-		}
-	}
-	panic("failed")
-}
-
-func possibleNewState(minCost map[posAndDir]int, s state, score int) bool {
-	pad := s.asPosAndDir()
-	if v, ok := minCost[pad]; !ok || score < v {
-		minCost[pad] = score
-		return true
-	}
-	return false
-}
-
 func part2(entries []string) int {
-	maze := common.ArraysGridFromLines(entries)
-	var start, end common.Point
-	for p := range maze.AllPoints() {
-		switch maze.Get(p) {
-		case 'S':
-			start = p
-		case 'E':
-			end = p
-		}
-	}
+	maze, start, end := readMaze(entries)
 	minCost := make(map[posAndDir]int)
 	bestPathCost := math.MaxInt
+	// All the points that are part of the optimal paths are in this set
 	allBestPathsPoints := mapset.NewThreadUnsafeSet[common.Point]()
 	pq := lane.NewMinPriorityQueue[state, int]()
 	pq.Push(state{[]common.Point{start}, common.E}, 0)
@@ -123,35 +121,26 @@ func part2(entries []string) int {
 		}
 		// Find all the possible new states
 		// Move forward
-		newPath := slices.Clone(curState.path)
-		newPath = append(newPath, curState.pos().Add(curState.dir))
-		newState := state{newPath, curState.dir}
-		newScore := score + 1
-		if v, ok := maze.CheckedGet(newState.pos()); ok && v != '#' {
-			if possibleNewState2(minCost, newState, newScore) {
-				pq.Push(newState, newScore)
+		p := curState.pos().Add(curState.dir)
+		if v, ok := maze.CheckedGet(p); ok && v != '#' {
+			newScore := score + 1
+			if potentialPath(minCost, posAndDir{p, curState.dir}, newScore, true) {
+				newPath := slices.Clone(curState.path)
+				newPath = append(newPath, p)
+				pq.Push(state{newPath, curState.dir}, newScore)
 			}
 		}
 		// Turn right
-		newState = state{curState.path, curState.dir.Right()}
-		newScore = score + 1000
-		if possibleNewState2(minCost, newState, newScore) {
+		newState := state{curState.path, curState.dir.Right()}
+		newScore := score + 1000
+		if potentialPath(minCost, newState.asPosAndDir(), newScore, true) {
 			pq.Push(newState, newScore)
 		}
 		// Or turn left
 		newState = state{curState.path, curState.dir.Left()}
-		if possibleNewState2(minCost, newState, newScore) {
+		if potentialPath(minCost, newState.asPosAndDir(), newScore, true) {
 			pq.Push(newState, newScore)
 		}
 	}
 	panic("failed")
-}
-
-func possibleNewState2(minCost map[posAndDir]int, s state, score int) bool {
-	pad := s.asPosAndDir()
-	if v, ok := minCost[pad]; !ok || score <= v {
-		minCost[pad] = score
-		return true
-	}
-	return false
 }
