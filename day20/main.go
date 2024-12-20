8db3d33f8db099accffb940af6794a6a85e94a92
Eric Lunt <eric@lunt.software>
2024-12-20 10:00:40 -0600

    Day 20 cleanup

--- day20/main.go
+++ day20/main.go
@@ -17,99 +17,81 @@ func main() {
 	fmt.Printf("Part 2: %d\n", part2(entries, 20, 100))
 }
 
-type cheat [2]common.Point
-
 func part1(entries []string, minTimeSaved int) int {
-	grid, start, end := readGrid(entries)
-	path := findPath(grid, start, end)
+	grid := common.ArraysGridFromLines(entries)
+	path := findPath(grid)
 	// The point on a path pointing to the picosecond
 	stepMap := make(map[common.Point]int)
-	for i, p := range path {
-		stepMap[p] = i
+	for tick, p := range path {
+		stepMap[p] = tick
 	}
-	validCheats := make(map[cheat]bool)
+	var validCheats int
 	// Now step along every point in the path and see if there are cheats
-	for i, p := range path[:len(path)-minTimeSaved-1] {
+	for tick, cheatStart := range path[:len(path)-minTimeSaved-1] {
 		// For each wall
-		for cheatStart := range p.SurroundingCardinals() {
-			if v, _ := grid.CheckedGet(cheatStart); v == '#' {
+		for p := range cheatStart.SurroundingCardinals() {
+			if v, _ := grid.CheckedGet(p); v == '#' {
 				// Now see if there are any shortcuts
-				for cheatEnd := range cheatStart.SurroundingCardinals() {
+				for cheatEnd := range p.SurroundingCardinals() {
 					if cheatEndStep, ok := stepMap[cheatEnd]; ok {
-						timeSaved := cheatEndStep - i - 2
+						timeSaved := cheatEndStep - tick - 2
 						if timeSaved >= minTimeSaved {
-							validCheats[[2]common.Point{cheatStart, cheatEnd}] = true
+							validCheats++
 						}
 					}
 				}
 			}
 		}
 	}
-	return len(validCheats)
+	return validCheats
 }
 
-func readGrid(entries []string) (grid common.Grid, start, end common.Point) {
-	grid = common.ArraysGridFromLines(entries)
-	for p := range grid.AllPoints() {
-		v := grid.Get(p)
-		if v == 'S' {
-			start = p
-		} else if v == 'E' {
-			end = p
+func findPath(grid common.Grid) []common.Point {
+	var p common.Point
+	for p = range grid.AllPoints() {
+		if grid.Get(p) == 'S' {
+			break
 		}
 	}
-	return
-}
-
-func findPath(grid common.Grid, start, end common.Point) []common.Point {
-	path := []common.Point{}
-	for p := start; p != end; p = nextPoint(grid, p) {
+	var path []common.Point
+	var previous common.Point
+	for {
 		path = append(path, p)
-		grid.Set(p, 'O')
-	}
-	path = append(path, end)
-	return path
-}
-
-func nextPoint(grid common.Grid, p common.Point) common.Point {
-	for sc := range p.SurroundingCardinals() {
-		if v, _ := grid.CheckedGet(sc); v == '.' || v == 'E' {
-			return sc
+		if grid.Get(p) == 'E' {
+			return path
+		}
+		for sc := range p.SurroundingCardinals() {
+			if v, _ := grid.CheckedGet(sc); (sc != previous && v == '.') || v == 'E' {
+				previous = p
+				p = sc
+				break
+			}
 		}
 	}
-	panic("No next point")
 }
 
+// This also works for part 1 if cheatLimit is set to 2
 func part2(entries []string, cheatLimit int, minTimeSaved int) int {
-	grid, start, end := readGrid(entries)
-	path := findPath(grid, start, end)
+	grid := common.ArraysGridFromLines(entries)
+	path := findPath(grid)
 	// The point on a path pointing to the picosecond
 	stepMap := make(map[common.Point]int)
 	for i, p := range path {
 		stepMap[p] = i
 	}
 	var validCheats int
-	// This is for deugging only . Time saved pointing to number of cheats
-	timeSavedMap := make(map[int]int)
 	// Now step along every point in the path and find adjacent potential cheat starts
 	for tick, cheatStart := range path[:len(path)-cheatLimit-1] {
-		// Check all downstream path points and see if we can reach there in 20 ticks or less
+		// Check all downstream path points and see if we can reach there in the time limit
 		for _, cheatEnd := range path[tick+cheatLimit:] {
 			dist := cheatEnd.Sub(cheatStart).ManhattanDistance()
 			if dist <= cheatLimit {
 				timeSaved := stepMap[cheatEnd] - tick - dist
 				if timeSaved >= minTimeSaved {
 					validCheats++
-					// For debug
-					timeSavedMap[timeSaved]++
 				}
 			}
 		}
-	}
-
-	// Debug
-	for k, v := range timeSavedMap {
-		fmt.Printf("There are %d cheats that save %d picoseconds\n", v, k)
 	}
 	return validCheats
 }
--- day20/main_test.go
+++ day20/main_test.go
@@ -16,11 +16,11 @@ func Test_part1(t *testing.T) {
 		args args
 		want int
 	}{
-		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 11},
+		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 5},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := part1(tt.args.entries, 19); got != tt.want {
+			if got := part1(tt.args.entries, 20); got != tt.want {
 				t.Errorf("part1() = %v, want %v", got, tt.want)
 			}
 		})
@@ -36,7 +36,7 @@ func Test_part2(t *testing.T) {
 		args args
 		want int
 	}{
-		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 31},
+		{"example", args{common.ReadStringsFromFile("testdata/example.txt")}, 32 + 31 + 29 + 39 + 25 + 23 + 20 + 19 + 12 + 14 + 12 + 22 + 4 + 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
